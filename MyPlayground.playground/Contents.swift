c25ea415dc94c8fc1153d370aff1daffbce5e7e4
Yunchen Wei <weiyunchen0807@outlook.com>
2015-07-23 23:28:50 -0400

    Daily Commit

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -1,20 +1,49 @@
-//: Playground - noun: a place where people can play
+func anyCommonElements <T, U where T: SequenceType, U: SequenceType, T.Generator.Element: Equatable, T.Generator.Element == U.Generator.Element> (lhs: T, rhs: U) -> [T.Generator.Element] {
+    var res: [T.Generator.Element] = []
+    for lhsItem in lhs {
+        for rhsItem in rhs {
+            if lhsItem == rhsItem {
+                res.append(lhsItem)
+            }
+        }
+    }
+    return res
+}
+anyCommonElements([1, 2, 3], [3])
 
-import UIKit
-
-var str = "Hello, playground"
-
-var message1 = "Hello Swift! How can I get started?"
-var message2 = "The best way to get started is to stop talking and code.";
 
 
-message1.uppercaseString
-message2.lowercaseString + "Okay, I'm working on it :)"
+var a = [String]()
+var b : [String] = []
 
-let messageLabel = UILabel(frame: CGRectMake(0, 0, 300, 50))
-messageLabel.text = message1
-messageLabel.backgroundColor = UIColor.orangeColor()
-messageLabel.textAlignment = NSTextAlignment.Center
-messageLabel.layer.cornerRadius = 10.0
-messageLabel.clipsToBounds = true
-messageLabel
+
+let cat = "🐱"; print(cat)
+print("🐱")
+
+
+enum Test<T> {
+    case None
+    case Some(T)
+}
+
+cat.startIndex.successor()
+
+let greeting = "Guten Tag!"
+
+var welcome = "hello"
+
+welcome.insert("!", atIndex: welcome.endIndex)
+
+//greeting.insert("ads", atIndex: greeting.endIndex)
+
+//func test(number: Int, number2: Int) -> Int{
+//    return 5
+//}
+//
+
+welcome.splice("asd", atIndex: welcome.endIndex)
+//test(6, 7)
+welcome
+
+let range = advance(welcome.endIndex, -6)..<welcome.endIndex
+welcome.removeRange(range)
