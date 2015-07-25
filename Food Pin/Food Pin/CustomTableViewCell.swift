78581de4ebe9184bb008afbe5b98470462febe5a
Yunchen Wei <weiyunchen0807@outlook.com>
2015-07-25 10:40:46 -0400

    Project 3: Create Custom Table View Cell

---
+++ Food Pin/Food Pin/CustomTableViewCell.swift
@@ -0,0 +1,30 @@
+
+//  CustomTableViewCell.swift
+//  FoodPin
+//
+//  Created by WEI, Yunchen on 7/25/15.
+//  Copyright (c) 2015 WEI, Yunchen. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+    
+    // MARK: Properties
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var locationLabel: UILabel!
+    @IBOutlet weak var typeLabel: UILabel!;
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
